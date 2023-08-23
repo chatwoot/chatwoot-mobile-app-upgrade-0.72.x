a4e0b3aebab89a14cbf7e943b84116c34fc5e845
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-08-23 10:52:18 +0530

    chore: Add assets

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import * as Sentry from '@sentry/react-native';
+// import Config from 'react-native-config';
 import {name as appName} from './app.json';
 
+import App from './App';
+
 AppRegistry.registerComponent(appName, () => App);
+// if (!__DEV__) {
+//   Sentry.init({
+//     dsn: Config.SENTRY_DSN,
+//     tracesSampleRate: 1.0,
+//     attachScreenshot: true,
+//   });
+// }
