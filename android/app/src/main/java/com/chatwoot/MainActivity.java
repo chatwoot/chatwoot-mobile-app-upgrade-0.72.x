269474f8db641d4340a6eb8d0ed2d81169f0a9d0
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-08-24 13:14:04 +0530

    feat: Splash screen (#2)

--- android/app/src/main/java/com/chatwoot/MainActivity.java
+++ android/app/src/main/java/com/chatwoot/MainActivity.java
@@ -1,9 +1,12 @@
 package com.chatwoot.app;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +17,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Chatwoot";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.BootTheme);
+    super.onCreate(savedInstanceState);
   }
 
   /**
